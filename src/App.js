30b5fd0e0f8bcc0d699784c989d2b1cdbec00be1
yRodrigo2219 <rodrigo2219dg@gmail.com>
2022-02-17 16:40:30 -0300

    Poke Hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import usePoke from "./hooks/usePoke";
 import "nes.css/css/nes.min.css";
 
 export default function App() {
+  const { data, isFetching, error } = usePoke(150, 0);
+
   return <span className="nes-text is-primary">PokéDex</span>;
 }
---
+++ src/hooks/usePoke.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+export default function usePoke(limit = 10, offset = 0) {
+  const [data, setData] = useState(null);
+  const [isFetching, setIsFetching] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((err) => setError(err))
+      .finally(() => setIsFetching(false));
+  }, []);
+
+  console.log(data);
+  return { data, isFetching, error };
+}
